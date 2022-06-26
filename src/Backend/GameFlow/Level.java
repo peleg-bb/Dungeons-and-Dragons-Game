e1930c1a7d5908b8519bfe42cc8eba4dc574c484
Peleg <benbapel@post.bgu.ac.il>
2022-06-26 15:48:26 +0300

    :champagne: Implemented GameFlow. Need to creade level reader and test

--- src/Backend/GameFlow/Level.java
+++ src/Backend/GameFlow/Level.java
@@ -17,20 +17,19 @@ public class Level {
     public boolean gameOver;
     public boolean done;
 
-    public Level(Player p, List<Enemy> t ){
+    public Level(Player p, List<Enemy> t) {
         this.p = p;
         this.enemies = t;
         gameOver = false;
         done = false;
     }
+
     public void GameTick(char choice) {
         p.onGameTick();
         move(choice);
         if (p.isDead()) {
-            gameOver = true;
-            // send game over message
-        }
-        else {
+            p.setMassageCallBack((msg) -> gameOver = true);
+        } else {
             for (Enemy e : enemies) {
                 e.onGameTick(p);
                 if (e.isDead()) {
@@ -42,29 +41,24 @@ public class Level {
             }
         }
     }
-
-
-    public void move(char move){
+    public void move(char move) {
         int x = p.getPosition().getX();
         int y = p.getPosition().getY();
-        Position point = new Position(x,y);
-        if(move == 'w'){
-            p.visit(board.getTile(x,y+1));
+        Position point = new Position(x, y);
+        if (move == 'w') {
+            p.visit(board.getTile(x, y + 1));
         }
-        if(move == 's'){
-            p.visit(board.getTile(x,y-1));
-        }
-        else if(move == 'd'){
-            p.visit(board.getTile(x+1,y));
-        }
-        else if(move == 'a'){
-            p.visit(board.getTile(x-1,y));
-        }
-        else if(move == 'c'){
+        if (move == 's') {
+            p.visit(board.getTile(x, y - 1));
+        } else if (move == 'd') {
+            p.visit(board.getTile(x + 1, y));
+        } else if (move == 'a') {
+            p.visit(board.getTile(x - 1, y));
+        } else if (move == 'c') {
             p.onAbilityCast(enemies);
         }
     }
-
+}
 //    private void FightEnemy() { // I believe we should use visitor pattern instead of this method
 //        for(Enemy e : enemies){
 //            if(e.position.equals(p.position)){
@@ -90,4 +84,3 @@ public class Level {
 //
 //        return false;
 //    }
-}
---
+++ src/Backend/GameFlow/LevelReader.java
@@ -0,0 +1,6 @@
+package Backend.GameFlow;
+
+// Reads a txt file and produces a level
+public class LevelReader {
+
+}
